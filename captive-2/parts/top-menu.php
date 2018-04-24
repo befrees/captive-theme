2b366f0c946f7f416b25d2203a2f95e22da2d0cb
DESKTOP-QNA868E\befre <befrees.svl@gmail.com>
2018-04-24 15:45:52 +0300

    del new

--- captive-2/parts/top-menu.php
+++
@@ -1,6 +0,0 @@
-<div class="super-menu">
-<div class="super-menu-container">
-	<?php wp_nav_menu(array( 'menu' => 'top-menu', 'container_class'=>'super-menu-inner scrollbar-inner has_scrollbar' )); ?>
-</div>
-	
-</div>
